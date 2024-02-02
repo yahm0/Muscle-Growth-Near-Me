106345e50bc79ba11b4086d8a42ab403e56691de
yahm0 <stnt190@outlook.com>
2024-02-01 20:27:40 -0500

    Adding the initial JavaScript file

---
+++ script.js
@@ -0,0 +1,7 @@
+// declaring a variable to generate a list with a btn click
+var generateBtn = document.querySelector("#generate");
+
+// Function to generate a list of Nutrient Supplements Stores
+function generateNstores() {
+
+}
