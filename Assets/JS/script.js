bcb5769f8b1cdfb7c3d40c674ff4b6b69e1ab535
Finian Corbett <finian97@gmail.com>
2024-02-07 22:39:52 -0500

    Added ArcGIS Maps API to script.js, ArcGIS CDNs for CSS and API to Map.html, and incorporated tailwind properties to Map.html to get the ArcGIS map onto the page

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -5,3 +5,21 @@ var generateBtn = document.querySelector("#generate");
 function generateNstores() {
 
 }
+
+// Base ArcGIS map
+require(["esri/config", "esri/Map", "esri/views/MapView"], function(esriConfig, Map, MapView) {
+
+    esriConfig.apiKey = "AAPKb79e717e9ee44bc7ad44bdff16503487YHN5UMlxJ3umTDOU5NvZbMyS-BtJxYC3zzyGhFfqbpuuSesI0Iz-XcLkG2yQLtim";
+
+    const map = new Map({
+      basemap: "arcgis/imagery" // basemap styles service
+    });
+
+    const view = new MapView({
+      map: map,
+      center: [-118.805, 34.027], // Longitude, latitude
+      zoom: 13, // Zoom level
+      container: "viewDiv" // Div element
+    });
+
+  });
