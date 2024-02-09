6ad89615af160acc3b5e56b92676d78af71f4c0f
yahm0 <stnt190@outlook.com>
2024-02-08 20:21:45 -0500

    Added function submitAddressForm(data) to the script.js file. I also made modifications to the readme.md file. Need to still add the api key and add additional logic to the fetch() function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,28 +1,12 @@
-// declaring a variable to generate a list with a btn click
-var generateBtn = document.querySelector("#generate");
+const arcgisApiKey = 'OUR_ARCGIS_API_KEY'; // Replace with our actual API key
+const arcgisUrl = 'https://api.arcgis.com/...'; // ArcGIS API endpoint finian this is for the fetch()
 
-// Function to generate a list of Nutrient Supplements Stores
-function generateNstores() {
-
+function submitAddressForm(data) {
+    // Logic to pass form data to ArcGIS API
+    // This can include geocoding the address and fetching nearby gym locations
 }
 
-// Base ArcGIS map
-require(["esri/config", "esri/Map", "esri/views/MapView"], function(esriConfig, Map, MapView) {
-
-    esriConfig.apiKey = "AAPKb79e717e9ee44bc7ad44bdff16503487YHN5UMlxJ3umTDOU5NvZbMyS-BtJxYC3zzyGhFfqbpuuSesI0Iz-XcLkG2yQLtim";
-
-    const map = new Map({
-      basemap: "arcgis/imagery" // basemap styles service
-    });
-
-    const view = new MapView({
-      map: map,
-      center: [-118.805, 34.027], // Longitude, latitude
-      zoom: 13, // Zoom level
-      container: "viewDiv" // Div element
-    });
-
-  });
+}
 
 document.getElementById('addressForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting the traditional way
